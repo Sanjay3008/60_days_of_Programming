5652a3615aa9b1864a174ea011dbcb2d614e29f6
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2022-05-05 08:13:38 +0530

    Create Move_negative_elements_to _left_and_positive_elements_to_right.py

---
+++ Move_negative_elements_to _left_and_positive_elements_to_right.py
@@ -0,0 +1,11 @@
+arr = [-1, 2, -3, 4, 5, 6, -7, 8, 9]
+
+j = 0
+
+for i in range(0,len(arr)-1,1):
+    if(arr[i]<0):
+        temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+        j=j+1
+    print(arr)
